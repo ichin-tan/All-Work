bfaf0a98eb4ca0a90fd559dba618afffd4b75a79
Coding Pirate <chintanpanchal396@gmail.com>
2025-03-26 18:41:30 -0400

    created react project and added firebase

---
+++ T440/React Native/firebase_application/config/FirebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDBX1UryQWVpC0CKVTuPeH96gCifPFD0s",
+  authDomain: "reactfire-b2188.firebaseapp.com",
+  projectId: "reactfire-b2188",
+  storageBucket: "reactfire-b2188.firebasestorage.app",
+  messagingSenderId: "661332845063",
+  appId: "1:661332845063:web:1a1152a28fdb55ed5a40a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
